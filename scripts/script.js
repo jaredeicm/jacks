ec067d535dbacd554f7b67b4397b096d5b102b53
jaredeicm <jaredeicm@gmail.com>
2022-12-06 07:31:53 -0300

    Escondendo a SIze Bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
-document.querySelector('.btn-menu').addEventListener('click', () => {
-  document.querySelectorAll('.nav-item-name').addClass('oculto');
+const d = element => document.querySelector(element);
+const da = element => document.querySelectorAll(element);
+
+d('.btn-menu').addEventListener('click', () => {
+  ocultaBar(da('.nav-item-name'));
+  ocultaBar(da('.btn-menu span'));
+  if (d('.btn-menu span').classList.contains('oculto')) {
+    d('.menu-lateral').classList.add('menu-lateral-oculto');
+  } else {
+    d('.menu-lateral').classList.remove('menu-lateral-oculto');
+  }
 });
+
+function ocultaBar(e) {
+  e.forEach(item => {
+    if (item.classList.contains('oculto')) {
+      item.classList.remove('oculto');
+    } else {
+      item.classList.add('oculto');
+    }
+  });
+}
